f52e7148688177465ff576a448c8dc36de0f0bfd
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-05 00:46:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 console.log(dodger.style.right);
+console.log(dodger.style.left); 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
