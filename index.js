ca9547a002e1a86b2cd0f5f4e9b5457fccbb653f
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-05 00:40:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+  let right = parseInt(rightNumbers, 10);
  
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+  if (right > 0) {
+    dodger.style.left = `${left + 1}px`;
   }
 }
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
   
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
