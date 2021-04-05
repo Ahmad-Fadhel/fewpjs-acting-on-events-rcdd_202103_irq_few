598bed2480110a31148f56dbc99c5d4ca399f3c2
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-05 00:31:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ dodger.style.backgroundColor = "#FF69B4";
 console.log(dodger.style.left); // "180px"
 console.log(dodger.style.bottom); // "0px"
 dodger.style.bottom = "0px";
+document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+});
